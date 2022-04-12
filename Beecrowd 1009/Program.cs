0b03c1b62c3d2b9a3d53278ad0ad85afba4ac8ee
kidoc <kidoc@DESKTOP-6T3Q9GP>
2022-04-11 23:07:24 -0300

    Add project files.

---
+++ Beecrowd 1009/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        double salario, vendas, comissao = 0.15, total;
+        string nome;
+
+        nome  = Console.ReadLine();
+        salario = double.Parse(Console.ReadLine());
+        vendas = double.Parse(Console.ReadLine());
+      
+
+        total = ((vendas * comissao) + (salario));
+
+
+        Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        Console.ReadLine();
+
+        
+    }
+}
